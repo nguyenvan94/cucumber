d59c85381bc7dccb40966e9bbac92fe165b12079
Van Nguyen <you@example.com>
2020-11-26 21:52:29 +0700

    Van commit

---
+++ 1/src/main/java/pageObject/LoginPage.java
@@ -0,0 +1,30 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class LoginPage {
+ public static WebDriver driver;
+ public static WebElement element;
+ 
+ public LoginPage(WebDriver driver){
+	 this.driver = driver;
+	 
+ }
+ public static WebElement user_name(){
+	 element = driver.findElement(By.xpath("//input[@name='UserName']"));
+	return element;
+	 
+ }
+ public static WebElement pass_word(){
+	 return element = driver.findElement(By.xpath("//input[@name='Password']"));
+ }
+	
+ public static WebElement login_button(){
+	 return element = driver.findElement(By.xpath("//button[@type='button']"));
+ }
+	
+ public static WebElement login_success(){
+	 return element = driver .findElement(By.xpath("//span[contains(.,'Trang chủ')]"));
+ }
+}
